a77fa150756d08d47883487a1135d77ae87d5d0b
zealic <zealic@gmail.com>
2011-06-29 22:35:14 +0800

    * Clean code.

--- src/Zealib.Build.Tests/Run-AllTests.cmd
+++ src/Zealib.Build.Tests/Run-AllTests.cmd
@@ -4,28 +4,33 @@ CALL :CHECK_ENV
 SET MSBUILD_TOOL=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 PUSHD "%~dp0"
   FOR /f "delims=;" %%i IN ('dir /B /AD-H') DO (
-    CALL :RUN_TEST %%i
-    ECHO.
+    ECHO %1 | findstr ^^[0-9][0-9][0-9]_ > NUL
+    IF %ERRORLEVEL% == 0 (
+      CALL :RUN_TEST %%i
+      ECHO %ERRORLEVEL%
+    )
   )
 POPD
+
+
+:END_SECTION
 ECHO.
 PAUSE
 goto :eof
 
 
 :RUN_TEST
+SET TEST_NAME=%1
+SET TEST_NAME=%TEST_NAME:~4%
+ECHO Running test "%TEST_NAME%"...
 PUSHD "%1"
-  ECHO %1 | findstr ^^[0-9][0-9][0-9]_ > NUL
-  IF NOT %ERRORLEVEL% == 0 goto :eof
-  SET TEST_NAME=%1
-  SET TEST_NAME=%TEST_NAME:~4%
-  ECHO Running test "%TEST_NAME%"...
-  
   %MSBUILD_TOOL% /nologo /v:minimal "%TEST_NAME%.proj"
   
   IF NOT %ERRORLEVEL% == 0 (
     ECHO Test "%TEST_NAME%" FAILED!
-    EXIT /B 1
+    ECHO.
+    PAUSE
+    EXIT 1
   )
   ECHO Test "%TEST_NAME%" PASS.
 POPD
