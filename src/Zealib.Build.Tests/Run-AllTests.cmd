359338c29e4498dc2a9ec259f0c101325934a07c
zealic <zealic@gmail.com>
2011-06-29 17:29:34 +0800

    * Improve test flow and added 002_RegisterAssemblyResolvePath test fixture.

--- src/Zealib.Build.Tests/Run-AllTests.cmd
+++ src/Zealib.Build.Tests/Run-AllTests.cmd
@@ -5,6 +5,7 @@ SET MSBUILD_TOOL=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 PUSHD "%~dp0"
   FOR /f "delims=;" %%i IN ('dir /B /AD-H') DO (
     CALL :RUN_TEST %%i
+    ECHO.
   )
 POPD
 ECHO.
@@ -14,8 +15,19 @@ goto :eof
 
 :RUN_TEST
 PUSHD "%1"
-  %MSBUILD_TOOL% /nologo /v:minimal
-  ECHO Test "%1" PASS.
+  ECHO %1 | findstr ^^[0-9][0-9][0-9]_ > NUL
+  IF NOT %ERRORLEVEL% == 0 goto :eof
+  SET TEST_NAME=%1
+  SET TEST_NAME=%TEST_NAME:~4%
+  ECHO Running test "%TEST_NAME%"...
+  
+  %MSBUILD_TOOL% /nologo /v:minimal "%TEST_NAME%.proj"
+  
+  IF NOT %ERRORLEVEL% == 0 (
+    ECHO Test "%TEST_NAME%" FAILED!
+    EXIT /B 1
+  )
+  ECHO Test "%TEST_NAME%" PASS.
 POPD
 goto :eof
 
