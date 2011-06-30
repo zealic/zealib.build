24feb20fd98238f343e7aa8848cdc02c54fecbf3
zealic <zealic@gmail.com>
2011-06-30 10:36:54 +0800

    * Using MSBuild to run tests.

--- src/Zealib.Build.Tests/Run-AllTests.cmd
+++ src/Zealib.Build.Tests/Run-AllTests.cmd
@@ -3,38 +3,13 @@ CALL :CHECK_ENV
 
 SET MSBUILD_TOOL=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 PUSHD "%~dp0"
-  FOR /f "delims=;" %%i IN ('dir /B /AD-H') DO (
-    ECHO %1 | findstr ^^[0-9][0-9][0-9]_ > NUL
-    IF %ERRORLEVEL% == 0 (
-      CALL :RUN_TEST %%i
-      ECHO %ERRORLEVEL%
-    )
-  )
+   ECHO Starting all tests...
+  "%MSBUILD_TOOL%" /nologo "Zealib.Build.Tests.proj"
 POPD
-
-
-:END_SECTION
 ECHO.
 PAUSE
 goto :eof
 
-
-:RUN_TEST
-SET TEST_NAME=%1
-SET TEST_NAME=%TEST_NAME:~4%
-ECHO Running test "%TEST_NAME%"...
-PUSHD "%1"
-  %MSBUILD_TOOL% /nologo /v:minimal "%TEST_NAME%.proj"
-  
-  IF NOT %ERRORLEVEL% == 0 (
-    ECHO Test "%TEST_NAME%" FAILED!
-    ECHO.
-    PAUSE
-    EXIT 1
-  )
-  ECHO Test "%TEST_NAME%" PASS.
-POPD
-goto :eof
 
 :CHECK_ENV
 IF NOT EXIST "%WINDIR%\Microsoft.NET\Framework\v4.0.30319" (
