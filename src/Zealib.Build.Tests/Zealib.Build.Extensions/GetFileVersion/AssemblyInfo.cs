4e228515e13f8561c8f21edfcd9f3104a46bf5f4
zealic <zealic@gmail.com>
2011-06-30 11:28:59 +0800

    + Added GetFileVersion test fixture.

---
+++ src/Zealib.Build.Tests/Zealib.Build.Extensions/GetFileVersion/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("3.4.5.6")]
+[assembly: AssemblyFileVersion("3.4.5.6")]
