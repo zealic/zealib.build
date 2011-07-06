dfd8e314692953b28d4539c85ee1cde7cc67866b
zealic <zealic@gmail.com>
2011-07-06 17:02:40 +0800

    + Added EmbedManifest test fixture and SetFileVersion_Managed test fixture.

---
+++ src/Zealib.Build.Tests/Zealib.Build.Tasks/SetFileVersion_Managed/src/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.3.1.4")]
+[assembly: AssemblyFileVersion("1.3.1.4")]
